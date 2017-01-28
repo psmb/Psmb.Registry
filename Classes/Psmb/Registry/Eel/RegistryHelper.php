5c571a5f234f12bb85db84267e29347655912ebc
Dominique Feyer <dfeyer@ttree.ch>
2017-01-28 13:05:04 +0100

    TASK: Apply migration Neos.Flow-20161124204700
    Adjusts code to package renaming from "TYPO3.Flow" to "Neos.Flow"

--- Classes/Psmb/Registry/Eel/RegistryHelper.php
+++ Classes/Psmb/Registry/Eel/RegistryHelper.php
@@ -1,7 +1,7 @@
 <?php
 namespace Psmb\Registry\Eel;
 
-use TYPO3\Flow\Annotations as Flow;
+use Neos\Flow\Annotations as Flow;
 use TYPO3\Eel\ProtectedContextAwareInterface;
 use Psmb\Registry\Service\RegistryService;
 

43b4972ad2e74166a848d8878d6daf76d1c6ff93
Dominique Feyer <dfeyer@ttree.ch>
2017-01-28 13:18:46 +0100

    TASK: Add support to increment and decrement counter

--- Classes/Psmb/Registry/Eel/RegistryHelper.php
+++ Classes/Psmb/Registry/Eel/RegistryHelper.php
@@ -1,8 +1,13 @@
 <?php
 namespace Psmb\Registry\Eel;
 
+<<<<<<< Updated upstream
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Eel\ProtectedContextAwareInterface;
+=======
+use Neos\Eel\ProtectedContextAwareInterface;
+use Neos\Flow\Annotations as Flow;
+>>>>>>> Stashed changes
 use Psmb\Registry\Service\RegistryService;
 
 /**
@@ -20,7 +25,8 @@ class RegistryHelper implements ProtectedContextAwareInterface
      * @param $key
      * @return mixed
      */
-    public function get($key) {
+    public function get($key)
+    {
         return $this->registryService->getRegister($key);
     }
 
@@ -28,8 +34,33 @@ class RegistryHelper implements ProtectedContextAwareInterface
      * @param $key
      * @param $value
      */
-    public function set($key, $value) {
+    public function set($key, $value)
+    {
         $this->registryService->setRegister($key, $value);
+    }
+
+    /**
+     * @param $key
+     * @return int
+     */
+    public function increment($key)
+    {
+        $currentValue = (int)$this->registryService->getRegister($key);
+        $nextValue = $currentValue < PHP_INT_MAX ? ++$currentValue : $currentValue;
+        $this->registryService->setRegister($key, $nextValue);
+        return $nextValue;
+    }
+
+    /**
+     * @param $key
+     * @return int
+     */
+    public function decrement($key)
+    {
+        $currentValue = (int)$this->registryService->getRegister($key);
+        $nextValue = $currentValue > 1 ? --$currentValue : 0;
+        $this->registryService->setRegister($key, $nextValue);
+        return $nextValue;
     }
 
     /**
